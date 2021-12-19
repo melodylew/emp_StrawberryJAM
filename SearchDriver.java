c923f9478f5d05654c441c9e311b0ec17c58db3d
sophiaei <seiden30@stuy.edu>
2021-12-18 22:06:06 -0500

    searchdriver.java

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,17 +1,73 @@
+/*
+0. Instantiate BinSearch and LinSearch
+1. populate( int[] a)
+  -for loop that iterates through the indices of a and fills a with its index
+
+
+*in binSearchTime( Comparable[] a, Comparable target)*
+1. long variables: start and finish
+  - start = currentTimeMillis
+2. Run BinSearch
+3. finish = currentTimeMillis
+4. return finish-start
+
+
+*in linSearchTime( Comparable[] a, Comparable target)*
+1. long variables: start and finish
+  - start = currentTimeMillis
+2. Run LinSearch
+3. finish = currentTimeMillis
+4. return finish-start
+
+
+**Use different array sizes to test**
+*/
+
 public class SearchDriver{
+  private static BinSearch bin = new BinSearch();
+  private static LinSearch lin = new LinSearch();
 
-  public void populate(int[] a){
-    //foo
-  }
-  public long binSearchTime(Comparable[] a, Comparable target){
-    return (long) 0;
+  public static void populate(Integer[] a){
+    for (int i = 0; i < a.length; i++){
+      a[i] = i;
+    }
   }
 
-  public long linSearchTime(Comparable[] a, Comparable target){
-    return (long) 0;
+  public static long binSearchTime(Comparable[] a, Comparable target){
+    long start, finish;
+    start = System.currentTimeMillis();
+
+    bin.binSearch(a, target);
+    finish = System.currentTimeMillis();
+
+    return finish - start;
+  }
+
+  public static long linSearchTime(Comparable[] a, Comparable target){
+    long start, finish;
+    start = System.currentTimeMillis();
+
+    lin.linSearch(a, target);
+    finish = System.currentTimeMillis();
+
+    return finish - start;
   }
 
   public static void main(String[] args) {
-    
+    Integer[] a = new Integer[1];
+    int i = 1;
+    while (i < 100_000_000){
+      long binAvg = 0;
+      long linAvg = 0;
+      a = new Integer[i];
+      populate(a);
+      for (int j = -1; j < i; j++){
+        binAvg += binSearchTime(a, j);
+        linAvg += linSearchTime(a,j);
+      }
+      System.out.println("binary = " + binAvg);
+      System.out.println("linary = " + linAvg);
+      i *= 10;
+    }
   }
 }
